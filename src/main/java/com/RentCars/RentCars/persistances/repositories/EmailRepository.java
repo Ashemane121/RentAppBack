a0c353bf9da3bfc0e1390c585bbaf77f621c6951
Ben Mahmoud Aziz <62435488+Ashemane121@users.noreply.github.com>
2023-03-02 20:34:58 +0100

    Revert "added password reset"

--- src/main/java/com/RentCars/RentCars/persistances/repositories/EmailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.RentCars.RentCars.persistances.repositories;
-
-public interface EmailRepository {/*
-    Email save(Email email);
-    List<Email> findAll();
-    Optional<Email> findById(Long id);
-    List<Email> findByRecipient(String recipient);
-    void delete(Email email);*/
-}
