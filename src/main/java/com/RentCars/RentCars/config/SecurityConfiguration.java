ae9b299d1c383e084ab38a651c86e7b400d4effb
Aziz Ben Mahmoud <benmahmoud.aziz121@gmail.com>
2023-03-10 23:49:32 +0100

    fixed CORS errors in security config

--- src/main/java/com/RentCars/RentCars/config/SecurityConfiguration.java
+++ src/main/java/com/RentCars/RentCars/config/SecurityConfiguration.java
@@ -11,6 +11,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -24,31 +29,40 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-        .csrf()
-        .disable()
-        .authorizeHttpRequests()
-        .requestMatchers(
-                "/api/auth/register",
-                "/api/auth/authenticate",
-                "/api/auth/checkEmail",
-                "/api/auth/admin/**",
-                "/api/posts/get/**"
-        )
-          .permitAll()
-        .anyRequest()
-          .authenticated()
-        .and()
-          .sessionManagement()
-          .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        .authenticationProvider(authenticationProvider)
-        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-        .logout()
-        .logoutUrl("/api/auth/logout")
-        .addLogoutHandler(logoutHandler)
-        .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
-    ;
+            .csrf().disable()
+            .cors().and() // enable CORS
+            .authorizeHttpRequests()
+            .requestMatchers(
+                    "/api/auth/register",
+                    "/api/auth/authenticate",
+                    "/api/auth/checkEmail",
+                    "/api/auth/admin/**",
+                    "/api/posts/get/**"
+            ).permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+            .logout()
+            .logoutUrl("/api/auth/logout")
+            .addLogoutHandler(logoutHandler)
+            .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
 
     return http.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("*")); // set allowed origins to *
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE")); // set allowed HTTP methods
+    configuration.setAllowedHeaders(List.of("*")); // set allowed headers to *
+    configuration.setExposedHeaders(List.of("Authorization")); // expose Authorization header
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
